8c98cdef9fa38b2489503061485f0801828036a0
username <vasanthkumars2025@gmail.com>
2023-04-04 19:37:02 +0530

    removed unwanted fiels

--- src/server.js
+++ src/server.js
@@ -1,38 +1,43 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import path from 'path';
-import dontenv from 'dotenv';
-import { fileURLToPath } from 'url';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import path from "path";
+import dontenv from "dotenv";
+import { fileURLToPath } from "url";
 
-import api from './routes/api.js';
+import api from "./routes/api.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 dontenv.config({
-    path: path.resolve('../.env')
+  path: path.resolve(".env"),
 });
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use('/', api);
+app.use("/", api);
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-})
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
+});
 
-mongoose.connect(MONGODB_URL, { 
-        useNewUrlParser: true, 
-    })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}\nMongoDB is connected`)))
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on port: ${PORT}\nMongoDB is connected`)
+    )
+  )
+  .catch((error) => console.log(error.message));
